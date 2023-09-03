982d7e6326640fe656c7faff344a01cff1b935e9
mlerma <mariolerma0396@gmail.com>
2023-09-03 12:03:30 -0500

    Se agrega --name como argumento del programa

---
+++ hola.py
@@ -0,0 +1,10 @@
+import argparse
+
+def say_hello(name="Mario Lerma"):
+print("Hello ", name, "!")
+
+parser = argparse.ArgumentParser(description='Say hello.')
+parser.add_argument('--name', type=str, required=False,
+metavar='name', help='your name', default='World')
+args = parser.parse_args()
+say_hello(args.name)
